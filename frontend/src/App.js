eefa59bb02e1e9e57e0f2253465f4e9132c5376d
ebrugulec <ebru@ebrugulec.com>
2019-11-30 23:02:20 +0300

    added welcome screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import 'antd/dist/antd.css';
+// import './App.css';
+import Welcome from './components/pages/Welcome/Welcome'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Welcome />
+      </div>
+      // <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    )
+  }}
+
 
 export default App;
