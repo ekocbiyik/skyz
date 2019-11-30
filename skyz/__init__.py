d380d872ec7727ebeaa3c10635b61519ce97e5e4
Assendis <olmezasude@gmail.com>
2019-11-30 19:40:40 +0300

    Migrations, models update.

---
+++ skyz/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'skyz.apps.Config'
--- skyz/models/model.py
+++ skyz/models/model.py
@@ -6,23 +6,31 @@ __author__ = 'assendis'
 class Ekonomi(BaseModel):
     class Meta:
         db_table = "ekonomi"
+        app_label = 'skyz'
 
 
 class Spor(BaseModel):
     class Meta:
         db_table = "spor"
+        app_label = 'skyz'
+
 
 
 class Politika(BaseModel):
     class Meta:
         db_table = "politika"
+        app_label = 'skyz'
+
 
 
 class Teknoloji(BaseModel):
     class Meta:
         db_table = "teknoloji"
+        app_label = 'skyz'
+
 
 
 class Sanat(BaseModel):
     class Meta:
         db_table = "sanat"
+        app_label = 'skyz'
